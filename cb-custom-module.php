f3ff6fac9b14464d817d4ace52c331b9aae7de0e
bacoords <bacoords@gmail.com>
2016-01-27 11:56:08 -0800

    initial shade

--- cb-custom-module.php
+++ cb-custom-module.php
@@ -21,21 +21,21 @@ function fl_load_module_examples() {
 }
 add_action( 'init', 'fl_load_module_examples' );
 
-/**
- * Custom fields
- */
-function fl_my_custom_field( $name, $value, $field ) {
-    echo '<input type="text" class="text text-full" name="' . $name . '" value="' . $value . '" />';
-}
-add_action( 'fl_builder_control_my-custom-field', 'fl_my_custom_field', 1, 3 );
-
-/**
- * Custom field styles and scripts
- */
-function fl_my_custom_field_assets() {
-    if ( class_exists( 'FLBuilderModel' ) && FLBuilderModel::is_builder_active() ) {
-        wp_enqueue_style( 'my-custom-fields', FL_MODULE_EXAMPLES_URL . 'assets/css/fields.css', array(), '' );
-        wp_enqueue_script( 'my-custom-fields', FL_MODULE_EXAMPLES_URL . 'assets/js/fields.js', array(), '', true );
-    }
-}
-add_action( 'wp_enqueue_scripts', 'fl_my_custom_field_assets' );
+///**
+// * Custom fields
+// */
+//function fl_my_custom_field( $name, $value, $field ) {
+//    echo '<input type="text" class="text text-full" name="' . $name . '" value="' . $value . '" />';
+//}
+//add_action( 'fl_builder_control_my-custom-field', 'fl_my_custom_field', 1, 3 );
+//
+///**
+// * Custom field styles and scripts
+// */
+//function fl_my_custom_field_assets() {
+//    if ( class_exists( 'FLBuilderModel' ) && FLBuilderModel::is_builder_active() ) {
+//        wp_enqueue_style( 'my-custom-fields', CB_CUSTOM_MODULE_URL . 'assets/css/fields.css', array(), '' );
+//        wp_enqueue_script( 'my-custom-fields', CB_CUSTOM_MODULE_URL . 'assets/js/fields.js', array(), '', true );
+//    }
+//}
+//add_action( 'wp_enqueue_scripts', 'fl_my_custom_field_assets' );
