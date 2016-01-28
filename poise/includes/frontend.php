27cdba04fc6a74dcad1d01f461f1d4c2fd43eec1
bacoords <bacoords@gmail.com>
2016-01-28 11:31:37 -0800

    poise updates

--- poise/includes/frontend.php
+++ poise/includes/frontend.php
@@ -7,28 +7,33 @@
  * $module An instance of your module class.
  * $settings The module's settings.
  *
- * Example: 
+ *  
  */
 
 ?>
-<!--
-<div class="fl-example-text">
-  <div style="background:url(<?php echo $settings->cb_shade_photo_field_src; ?>) no-repeat center; background-size:cover;">
-		<a href="<?php echo $settings->cb_shade_link_field; ?>">
-			<?php echo $settings->cb_shade_editor_field; ?>
-		</a>
-	</div>
-</div>
--->
+
 	
-   <div class="cb-poise-row">
-    <?php foreach($settings->cb_poise_multiple_photos_field as $img) { 
-		 $src = wp_get_attachment_image_src($img, 'full');
-		 ?>
-     <div class="cb-poise-col">
-       <a href="<?php echo $src[0]; ?>" target="_self">
-				 <?php echo wp_get_attachment_image($img, 'full', false, array('class'=>'cb-poise-img')); ?>
-       </a>
-     </div>
-		<?php } ?>
+   <div class="cb-poise-container">
+   	<div class="cb-poise-row">
+			<?php 
+			$i = 0;
+			foreach($settings->cb_poise_multiple_photos_field as $img) { 
+			 $src = wp_get_attachment_image_src($img, 'full');
+			 ?>
+			 <div class="cb-poise-col">
+				 <a href="<?php echo $src[0]; ?>" target="_self">
+					 <?php echo wp_get_attachment_image($img, 'full', false, array('class'=>'cb-poise-img')); ?>
+				 </a>
+			 </div>
+			<?php 
+			$i++;
+				if($i == $settings->cb_poise_text_field){
+				?>
+				 </div>
+				 <div class="cb-poise-row"> 
+				<?php
+				}
+			
+			} ?>
+		 </div>
    </div>
--- shade/includes/frontend.php
+++ shade/includes/frontend.php
@@ -7,19 +7,10 @@
  * $module An instance of your module class.
  * $settings The module's settings.
  *
- * Example: 
+ *
  */
 
 ?>
-<!--
-<div class="fl-example-text">
-  <div style="background:url(<?php echo $settings->cb_shade_photo_field_src; ?>) no-repeat center; background-size:cover;">
-		<a href="<?php echo $settings->cb_shade_link_field; ?>">
-			<?php echo $settings->cb_shade_editor_field; ?>
-		</a>
-	</div>
-</div>
--->
 
 
 	<div class="cb-shade-boxes">
