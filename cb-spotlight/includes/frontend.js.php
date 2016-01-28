b6fb9ca14b31867161e598c32f9429cd3f299a57
bacoords <bacoords@gmail.com>
2016-01-28 14:14:06 -0800

    Spotlight

--- cb-spotlight/includes/frontend.js.php
+++ cb-spotlight/includes/frontend.js.php
@@ -6,10 +6,12 @@ jQuery(document).ready(function(){
   
   
   jQuery('.cb-spotlight-spn-link-<?php echo $id; ?>').click(function(){
+    jQuery('#cb-spotlight-spn-img-<?php echo $id; ?>').css('opacity','0.3');
     var i = jQuery(this).data('img-src');
-    jQuery('#cb-spotlight-spn-img-<?php echo $id; ?>').css('opacity','0.3').attr('src',i).css('opacity','1');
+    jQuery('#cb-spotlight-spn-img-<?php echo $id; ?>').load(function(){
+    	jQuery(this).css('opacity','1');
+    }).attr('src',i);
   });
   
-  
-  
+
 });
