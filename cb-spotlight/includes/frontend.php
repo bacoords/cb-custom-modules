26448b40ef6a5d96ea70b7ac490c4efba1c12ca4
bacoords <bacoords@gmail.com>
2016-01-28 13:28:52 -0800

    add spotlight

--- cb-spotlight/includes/frontend.php
+++ cb-spotlight/includes/frontend.php
@@ -15,14 +15,14 @@
 	<div class="cb-spotlight-spn-gallery">
 
       <div class="cb-spotlight-spn-image">
-       <img src="<?php echo $settings->cb_spotlight_form_field_repeater[0]->cb_spotlight_photo_field_src; ?>" alt="" id="cb-spotlight-spn-img"/>
+       <img src="<?php echo $settings->cb_spotlight_form_field_repeater[0]->cb_spotlight_photo_field_src; ?>" alt="" id="cb-spotlight-spn-img-<?php echo $id; ?>"/>
       </div>
 
 
       <div class="cb-spotlight-spn-links">
 					<?php foreach($settings->cb_spotlight_form_field_repeater as $current_form){	?>
 
-							 <a class="cb-spotlight-spn-link" data-img-src="<?php echo $current_form->cb_spotlight_photo_field_src; ?>"><?php echo $current_form->cb_spotlight_text_field; ?></a>
+							 <a class="cb-spotlight-spn-link cb-spotlight-spn-link-<?php echo $id; ?>" data-img-src="<?php echo $current_form->cb_spotlight_photo_field_src; ?>"><?php echo $current_form->cb_spotlight_text_field; ?></a>
 					<?php	} ?>
   		</div>
 
