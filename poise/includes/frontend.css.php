2586d7ac412f5284546e5998e785b7744075caa8
bacoords <bacoords@gmail.com>
2016-01-27 16:09:36 -0800

    add poise

---
+++ poise/includes/frontend.css.php
@@ -0,0 +1,29 @@
+/**
+ * This file should contain frontend styles that 
+ * will be applied to individual module instances.
+ *
+ * You have access to three variables in this file: 
+ * 
+ * $module An instance of your module class.
+ * $id The module's ID.
+ * $settings The module's settings.
+ *
+ * Example: 
+ */
+
+<?php if($settings->cb_shade_text_field) { ?>
+
+.fl-node-<?php echo $id; ?> .cb-shade-boxes .cb-shade-box-wrapper {
+    min-height: <?php echo $settings->cb_shade_text_field; ?>px;
+}
+
+.fl-node-<?php echo $id; ?> .cb-shade-boxes .cb-shade-box-wrapper .cb-shade-box-overlay {
+    min-height: <?php echo $settings->cb_shade_text_field; ?>px;
+}
+
+.fl-node-<?php echo $id; ?> .cb-shade-boxes .cb-shade-box-wrapper .cb-shade-box-overlay .cb-shade-box-inner {
+    min-height: <?php echo $settings->cb_shade_text_field; ?>px;
+}
+
+
+<?php } ?>
---
+++ poise/includes/frontend.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file should be used to render each module instance.
+ * You have access to two variables in this file: 
+ * 
+ * $module An instance of your module class.
+ * $settings The module's settings.
+ *
+ * Example: 
+ */
+
+?>
+<!--
+<div class="fl-example-text">
+  <div style="background:url(<?php echo $settings->cb_shade_photo_field_src; ?>) no-repeat center; background-size:cover;">
+		<a href="<?php echo $settings->cb_shade_link_field; ?>">
+			<?php echo $settings->cb_shade_editor_field; ?>
+		</a>
+	</div>
+</div>
+-->
+	
+   <div class="cb-poise-row">
+    <?php foreach($settings->cb_poise_multiple_photos_field => $img) { ?>
+     <div class="cb-poise-col">
+       <img src="<?php wp_get_attachment_image($img, 'full'); ?>" alt="App Screenshot" class="cb-poise-img">
+     </div>
+		<?php } ?>
+   </div>
