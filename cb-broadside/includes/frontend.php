5a11a130f71fa8756d8dd31b1682f4b15481721b
bacoords <bacoords@gmail.com>
2016-03-17 10:14:20 -0700

    add broadside

--- cb-broadside/includes/frontend.php
+++ cb-broadside/includes/frontend.php
@@ -14,9 +14,9 @@
 
  <?php if($settings->cb_broadside_multiple_photos_field){ ?> 	
     <div class="cb-broadside">
-      <span class="scroll-left">&#10094;</span>
-      <span class="scroll-right">&#10095;</span>
-      <ul>
+      <span class="scroll-left-<?php echo $id; ?>">&#10094;</span>
+      <span class="scroll-right-<?php echo $id; ?>">&#10095;</span>
+      <ul class="ul-<?php echo $id; ?>">
           <?php 
           foreach($settings->cb_broadside_multiple_photos_field as $img) { 
            $src = wp_get_attachment_image_src($img, 'full');
