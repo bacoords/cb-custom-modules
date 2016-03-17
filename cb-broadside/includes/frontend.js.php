861f8d68e20bd54067e550cb6ca61167d821639d
bacoords <bacoords@gmail.com>
2016-03-17 11:54:10 -0700

    add broadside

--- cb-broadside/includes/frontend.js.php
+++ cb-broadside/includes/frontend.js.php
@@ -1,23 +1,25 @@
 
 
 
- jQuery(document).ready(function(){
+jQuery(document).ready(function(){
   
   //Swap out images on link
   
-jQuery('.scroll-right-<?php echo $id; ?>').click(function(){
-  jQuery('.ul-<?php echo $id; ?>').animate( { scrollLeft: '+=500' }, 300);
-});
-jQuery('.scroll-left-<?php echo $id; ?>').click(function(){
-  jQuery('.ul-<?php echo $id; ?>').animate( { scrollLeft: '-=500' }, 300);
-});
+  jQuery('.scroll-right-<?php echo $id; ?>').click(function(){
+    jQuery('.ul-<?php echo $id; ?>').animate( { scrollLeft: '+=500' }, 300);
+  });
+  jQuery('.scroll-left-<?php echo $id; ?>').click(function(){
+    jQuery('.ul-<?php echo $id; ?>').animate( { scrollLeft: '-=500' }, 300);
+  });
 
-jQuery(function() {
-	jQuery('.cb-broadside-img').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		closeBtnInside: false
-	});
-});
+  jQuery(function() {
+    jQuery('.ul-<?php echo $id; ?>').magnificPopup({
+     delegate: a,
+     gallery:{enabled:true},
+      type: 'image',
+      closeOnContentClick: true,
+      closeBtnInside: false
+    });
+  });
 
 });
