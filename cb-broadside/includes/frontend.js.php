62855966f7090157d27bcaed5bf73db21f3626c6
bacoords <bacoords@gmail.com>
2016-03-17 10:33:16 -0700

    add broadside

--- cb-broadside/includes/frontend.js.php
+++ cb-broadside/includes/frontend.js.php
@@ -9,11 +9,12 @@ jQuery('.scroll-left-<?php echo $id; ?>').click(function(){
   jQuery('.ul-<?php echo $id; ?>').animate( { scrollLeft: '-=500' }, 300);
 });
 
+jQuery(function() {
 	jQuery('.cb-broadside-img').magnificPopup({
 		type: 'image',
 		closeOnContentClick: true,
 		closeBtnInside: false
 	});
-
+});
 
 });
