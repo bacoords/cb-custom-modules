4d695ca29cb64316a399db530b70c8bad62da3c6
bacoords <bacoords@gmail.com>
2016-06-15 10:33:37 -0700

    final push before merge

--- cb-custom-module.php
+++ cb-custom-module.php
@@ -3,7 +3,7 @@
  * Plugin Name: CB Custom Beaver Builder Modules 
  * Plugin URI: http://www.briancoords.com/free-beaver-builder-custom-modules
  * Description: Custom photographic modules for Beaver Builder.
- * Version: 1.0
+ * Version: 1.1.0
  * Author: Brian Coords
  * Author URI: http://www.briancoords.com
  */
