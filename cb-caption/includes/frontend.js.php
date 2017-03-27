cc4f77bd2ef77e102668935ef18a94fe369cbb34
bacoords <bacoords@gmail.com>
2017-03-27 12:34:47 -0700

    caption add mobile support v6

--- cb-caption/includes/frontend.js.php
+++ cb-caption/includes/frontend.js.php
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-    $('.cb-caption-box-wrapper').bind('touchstart touchend', function(e) {
+jQuery(document).ready(function() {
+    jQuery('.cb-caption-box-wrapper').bind('touchstart touchend', function(e) {
         e.preventDefault();
-        $(this).toggleClass('hover_effect');
+        jQuery(this).toggleClass('hover_effect');
     });
 });
