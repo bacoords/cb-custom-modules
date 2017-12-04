1c5f27d90c678a2cc9111e9f7d80972d199b5ea1
Brian Coords <bacoords@gmail.com>
2017-12-04 09:23:48 -0800

    fix btn_bg_opacity php error

--- cb-button-list/includes/frontend.php
+++ cb-button-list/includes/frontend.php
@@ -2,22 +2,22 @@
 
 /**
  * This file should be used to render each module instance.
- * You have access to two variables in this file: 
- * 
+ * You have access to two variables in this file:
+ *
  * $module An instance of your module class.
  * $settings The module's settings.
  *
- *  
+ *
  */
 
 ?>
-	
+
 <div class="cb-button-list">
-	
+
 	<?php $i = 0; ?>
 
 	<?php foreach($settings->cb_button_list_form_field_repeater as $button){ ?>
-			
+
 		<div class="cb-button-list--button cb-button-list--button-<?php echo $i; $i++; ?>">
 
 			<?php
@@ -32,20 +32,21 @@
 			} else {
 				$button_url = $button->cb_link_link;
 			}
+			$btn_bg_opacity = isset($button->btn_bg_opacity) ? $button->btn_bg_opacity : '0';
 
 			$btn_settings = array(
-				'align'				=> $module->filter_value( $button, 'btn_align' ),
+				'align'							=> $module->filter_value( $button, 'btn_align' ),
 				'bg_color'          => $module->filter_value( $button, 'btn_bg_color' ),
 				'bg_hover_color'    => $module->filter_value( $button, 'btn_bg_hover_color' ),
-				'bg_opacity'        => $module->filter_value( $button, 'btn_bg_opacity' ),
+				'bg_opacity'        => $btn_bg_opacity,
 				'border_radius'     => $module->filter_value( $button, 'btn_border_radius' ),
 				'border_size'       => $module->filter_value( $button, 'btn_border_size' ),
 				'font_size'         => $module->filter_value( $button, 'btn_font_size' ),
 				'icon'              => $button->btn_icon,
 				'icon_position'     => $button->btn_icon_position,
-				'icon_animation'	=> $button->btn_icon_animation,
+				'icon_animation'		=> $button->btn_icon_animation,
 				'link'              => $button_url,
-				'link_nofollow' 	=> $button->btn_link_nofollow,
+				'link_nofollow' 		=> $button->btn_link_nofollow,
 				'link_target'       => $button->btn_link_target,
 				'padding'           => $module->filter_value( $button, 'btn_padding' ),
 				'style'             => $module->filter_value( $button, 'btn_style' ),
@@ -60,7 +61,7 @@
 		?>
 
 		</div>
-		
+
 	<?php } ?>
 
 </div>
